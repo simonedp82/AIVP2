007dc99b99c0125a6dbe4946b163b53744678e1d
Simone <simonedp1982@gmail.com>
2023-01-23 14:26:19 +0100

    Update Test1.cs

--- Assets/Scripts/Test1.cs
+++ Assets/Scripts/Test1.cs
@@ -7,7 +7,7 @@ public class Test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int myNumber = 1;
     }
 
     // Update is called once per frame

4ba25af07dea67363361d86e12795071e59b6ef2
Meoow82 <simonedp1982@gmail.com>
2023-01-25 15:09:09 +0100

    Endless

--- Assets/Scripts/ProceduralLevel.cs
+++ Assets/Scripts/ProceduralLevel.cs
@@ -18,11 +18,18 @@ public class ProceduralLevel : MonoBehaviour
     public List<GameObject> sections;
     public float sectionLenght;
     public int maxSections;
+    public Transform tileContainer;
+
+    [Header("spawn Control")]
+    public Transform cameraTransform;
+    public float zCameraPosition;
+    public float lastBlockzPosition;
     
     // Start is called before the first frame update
     void Start()
     {
         spawnEndless();
+        cameraTransform = Camera.main.transform;
 
         //generateObjects();
     }
@@ -30,7 +37,21 @@ public class ProceduralLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        zCameraPosition = cameraTransform.position.z;
+        lastBlockzPosition = tileContainer.GetChild(tileContainer.childCount -3).transform.position.z;
+        if(zCameraPosition >= lastBlockzPosition)
+        {
+            instantiateNewTile(tileContainer.GetChild(tileContainer.childCount - 1).position.z);
+
+        }
+    }
+
+    public void instantiateNewTile(float zPosition)
+    {
+        int sectionIndex = Random.Range(0, sections.Count);
+        GameObject go = Instantiate(sections[sectionIndex], tileContainer);
+        go.transform.localPosition = new Vector3(0, 0, zPosition + sectionLenght);
+        Destroy(tileContainer.GetChild(0).gameObject);
     }
 
     public void generateObjects()
@@ -56,8 +77,8 @@ public class ProceduralLevel : MonoBehaviour
         for (int i = 0; i < maxSections; i++)
         {
             int sectionIndex = Random.Range(0, sections.Count);
-            GameObject go = Instantiate(sections[sectionIndex]);
-            go.transform.position = new Vector3(0, 0, sectionLenght * i);
+            GameObject go = Instantiate(sections[sectionIndex], tileContainer);
+            go.transform.localPosition = new Vector3(0, 0, sectionLenght * i);
         }
     }
 }
